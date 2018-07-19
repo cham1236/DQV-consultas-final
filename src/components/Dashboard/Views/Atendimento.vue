927bc3cb5d5f0ec21cbb1aa3805e427f6ce96568
Alessandro <alessandrolimabcc@gmail.com>
2018-07-19 10:10:01 -0300

    Alteração Login
    ajustes também no componente atendimento

--- src/components/Dashboard/Views/Atendimento.vue
+++ src/components/Dashboard/Views/Atendimento.vue
@@ -39,41 +39,7 @@
   import LTable from 'src/components/UIComponents/Table.vue'
   import Card from 'src/components/UIComponents/Cards/Card.vue'
   const tableColumns = ['Id', 'Name', 'Salary', 'Country', 'City']
-  const tableData = [{
-    id: 1,
-    name: 'Dakota Rice',
-    salary: '$36.738',
-    country: 'Niger',
-    city: 'Oud-Turnhout'
-  },
-  {
-    id: 2,
-    name: 'Minerva Hooper',
-    salary: '$23,789',
-    country: 'Curaçao',
-    city: 'Sinaai-Waas'
-  },
-  {
-    id: 3,
-    name: 'Sage Rodriguez',
-    salary: '$56,142',
-    country: 'Netherlands',
-    city: 'Baileux'
-  },
-  {
-    id: 4,
-    name: 'Philip Chaney',
-    salary: '$38,735',
-    country: 'Korea, South',
-    city: 'Overland Park'
-  },
-  {
-    id: 5,
-    name: 'Doris Greene',
-    salary: '$63,542',
-    country: 'Malawi',
-    city: 'Feldkirchen in Kärnten'
-  }]
+  const tableData = [{}]
   export default {
     components: {
       LTable,
@@ -118,27 +84,19 @@
           {value: 'Alice', text: 'Alice'},            
         ],
         
-      value : '',
-      shortcuts: [
-        {
-          text: 'Today',
-          onClick: () => {
-            this.time3 = [ new Date(), new Date() ]
+        value : '',
+        shortcuts: [
+          {
+            text: 'Today',
+            onClick: () => {
+              this.time3 = [ new Date(), new Date() ]
+            }
           }
-        }
-      ],
-      timePickerOptions:{
-        start: '00:00',
-        step: '00:30',
-        end: '23:30'
-      },
-        table1: {
-          columns: [...tableColumns],
-          data: [...tableData]
-        },
-        table2: {
-          columns: [...tableColumns],
-          data: [...tableData]
+        ],
+        timePickerOptions:{
+          start: '00:00',
+          step: '00:30',
+          end: '23:30'
         }
       }
     }
--- src/components/GeneralViews/Login.vue
+++ src/components/GeneralViews/Login.vue
@@ -1,30 +1,110 @@
 <template>
-   <div  class="back">
-      <div class="div-center">
-         <div class="content">
+<!--    <b-container fluid class="back wrapper content">
+      <b-row  align-v="center">
+         <b-col class="div-center">
+           <div>
+             <b-img center fluid src="static/img/Brasão_UFRPE.png" id="logo"></b-img>
+             <h1><strong>DQV</strong></h1>
+             <h3>{{msg}}</h3>
+           </div>
             <b-form-group>
                <b-form-input type="text"
                         placeholder="CPF"></b-form-input>
+                
                <b-form-input type="password"
                            placeholder="Senha"></b-form-input>
                <b-btn variant="primary" href="#/admin/noticias">Login</b-btn>
             </b-form-group>
-         </div>
+         </b-col>
+         <div class="w-100"></div>
+         <b-col>
+          <content-footer></content-footer>
+        </b-col>
+      </b-row>
 
-      </div>
-   </div>
-  
+      <b-row align-v="end" no-gutters="true">
+      </b-row>
+   </b-container>
+   -->
+      <b-container>
+        <b-row>
+            <b-col md="4" offset-md="4">
+                <b-card title="DQV Consultas"
+                        class="login-panel panel panel-default text-center">
+                    <div class="panel-body">
+                        <b-form-fieldset>
+
+                                <b-form-group>
+                                    <b-form-input class="form-control" placeholder=" CPF" name="cpf" type="text" autofocus></b-form-input>
+                                </b-form-group>
+                                <b-form-group>
+                                    <b-form-input placeholder="Password" name="password" type="password" value=""></b-form-input>
+                                </b-form-group>
+                                
+                                <!-- Change this to a button or input when using this as a form -->
+                                <b-btn block="true" variant="success" >Login</b-btn>
+                            
+                        </b-form-fieldset>
+                    </div>
+                </b-card>
+            </b-col>
+        </b-row>
+      </b-container>
 </template>
 
 <script>
-
+  import ContentFooter from 'src/components/Dashboard/Layout/ContentFooter.vue'
 export default {
+ components: {
+   ContentFooter
+ },
+ data (){
+   return{
+     msg: 'Módulo de Consultas, Bem Vindo(a)!'
+   }
+ }
   
 }
 </script>
 
 <style>
-.back {
+body {
+  background-color: #f8f8f8;
+}
+#wrapper {
+  width: 100%;
+}
+#page-wrapper {
+  padding: 0 15px;
+  min-height: 568px;
+  background-color: white;
+}
+@media (min-width: 768px) {
+  #page-wrapper {
+    position: inherit;
+    margin: 0 0 0 250px;
+    padding: 0 30px;
+    border-left: 1px solid #e7e7e7;
+  }
+}
+.login-panel {
+  margin-top: 25%;
+}
+.btn-success {
+  color: #5cb85c;
+}
+.panel-default {
+    -moz-box-shadow:    -3x 7px 5px 2px #ccc;
+  -webkit-box-shadow: 5px 7px 5px 2px #ccc;
+  box-shadow:        5 px 7px 5px 2px #ccc;
+}
+
+.panel-body {
+    padding: 20px;
+}
+
+
+/* .back {
   background: #e2e2e2;
   width: 100%;
   position: absolute;
@@ -54,5 +134,12 @@ div.content {
   display: table-cell;
   vertical-align: middle;
 }
+
+#logo{
+  height: 50%;
+  width: 25%;
+  padding-bottom: 30px;
+} */
+
 </style>
 
