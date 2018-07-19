623918e45724deaf62ccad0b7d824649c8307f18
Carlos <azevedo1236@gmail.com>
2018-07-19 14:07:39 -0300

    Mudanças diversas

--- src/components/Dashboard/Views/Atendimento.vue
+++ src/components/Dashboard/Views/Atendimento.vue
@@ -39,41 +39,6 @@
   import LTable from 'src/components/UIComponents/Table.vue'
   import Card from 'src/components/UIComponents/Cards/Card.vue'
   const tableColumns = ['Id', 'Name', 'Salary', 'Country', 'City']
-  const tableData = [{
-    id: 1,
-    name: 'Dakota Rice',
-    salary: '$36.738',
-    country: 'Niger',
-    city: 'Oud-Turnhout'
-  },
-  {
-    id: 2,
-    name: 'Minerva Hooper',
-    salary: '$23,789',
-    country: 'Curaçao',
-    city: 'Sinaai-Waas'
-  },
-  {
-    id: 3,
-    name: 'Sage Rodriguez',
-    salary: '$56,142',
-    country: 'Netherlands',
-    city: 'Baileux'
-  },
-  {
-    id: 4,
-    name: 'Philip Chaney',
-    salary: '$38,735',
-    country: 'Korea, South',
-    city: 'Overland Park'
-  },
-  {
-    id: 5,
-    name: 'Doris Greene',
-    salary: '$63,542',
-    country: 'Malawi',
-    city: 'Feldkirchen in Kärnten'
-  }]
   export default {
     components: {
       LTable,
@@ -131,18 +96,14 @@
         start: '00:00',
         step: '00:30',
         end: '23:30'
-      },
-        table1: {
-          columns: [...tableColumns],
-          data: [...tableData]
-        },
-        table2: {
-          columns: [...tableColumns],
-          data: [...tableData]
-        }
       }
+      }
+    },
+    methods:{
+      
     }
   }
 </script>
+
 <style>
 </style>
