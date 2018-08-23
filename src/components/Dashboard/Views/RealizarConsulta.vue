3f381e713e855bc4b6ef5d326c12efa840a17f9d
Carlos <azevedo1236@gmail.com>
2018-08-23 14:24:54 -0300

    aaa

--- src/components/Dashboard/Views/RealizarConsulta.vue
+++ src/components/Dashboard/Views/RealizarConsulta.vue
@@ -13,8 +13,24 @@
               <br>
               <b-btn variant="primary" block v-on:click="pegarHorarios">Pesquisar Paciente</b-btn>
 
+            <div v-for="consulta in consultas" :key="consulta">
 
-                         
+                <b-card title="" sub-title="">
+                    <h3 slot="header" class="card-title">{{consulta.consulta.especialista.especialidade}}</h3>
+                    <h5 v-if="consulta.consulta.especialista.especialidade==='MEDICO'" slot="header" class="card-title">{{consulta.consulta.especialista.tipo}}</h5>
+                    <p>Dr {{consulta.consulta.especialista.nome}}</p>
+                    <p v-if="consulta.consulta.status? status ='Realizada' : status ='Pendente' " >Status: {{status}}</p>
+                    <p class="card-text">
+                        Data: {{consulta.consulta.horario.diaria.diaria}}
+                    </p>
+                    <p class="card-text">
+                        Codigo: {{consulta.consulta.id}}
+                    </p>
+                    <div class="row">  
+                        <div class="col-md-10"><b-button class="card-link" v-on:click="verDetalhe (consulta.consulta.id)" >Detalhes</b-button></div>
+                    </div>
+                </b-card>
+            </div>                       
           </card>
         </div>
       </div>
